11167ff66614c9fdddeb8ae6a833990ef9107cd8
Jack <jconnolly444@gmail.com>
2023-12-06 14:24:17 -0600

    updated url to display favorite artist's info

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,6 @@
 class ArtistsController < ApplicationController
   def index
-    response = HTTP.get("https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Cher&api_key=#{ENV["LAST_FM_KEY"]}&format=json")
+    response = HTTP.get("https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=ween&api_key=#{ENV["LAST_FM_KEY"]}&format=json")
     data = JSON.parse(response)
     render json: data
   end
